d967a7a529019987f6817bad69e5ca4e60d9fb80
mcgear <michael.gearhardt@gmail.com>
2024-11-15 01:32:29 -0700

    github utils

---
+++ src/utils/.deps.ts
@@ -0,0 +1,13 @@
+export { delay } from "jsr:@std/async@1.0.3/delay";
+
+export * from "npm:octokit@4.0.2";
+export { type OctokitOptions } from "npm:@octokit/core@6.1.2";
+export * from "npm:@octokit/auth-app@6.0.1";
+// export { } from 'https://esm.sh/@octokit/auth-oauth-user@4.0.1';
+export * from "npm:@octokit/auth-token@5.1.1";
+export { type components } from "npm:@octokit/openapi-types@22.2.0/types.d.ts";
+export * from "npm:@octokit/plugin-rest-endpoint-methods@13.2.4";
+
+export { SecretClient } from "npm:@azure/keyvault-secrets@4.8.0";
+
+export type { EaCSourceDetails } from "../sources/.exports.ts";
---
+++ src/utils/.exports.ts
@@ -0,0 +1,11 @@
+/**
+ * Helpers for interacting with and managing GitHub through Octokit.
+ * @module
+ */
+export * from "./configureBranchProtection.ts";
+export * from "./configureRepository.ts";
+export * from "./getBranch.ts";
+export * from "./getOrCreateRepository.ts";
+export * from "./listBranches.ts";
+export * from "./tryGetRepository.ts";
+export * from "./types.ts";
