d2eec4444ab975338e9a800cb6139e4ce42e63f2
mcgear <michael.gearhardt@gmail.com>
2025-11-30 17:37:27 -0700

    ref updates

--- src/utils/.deps.ts
+++ src/utils/.deps.ts
@@ -7,8 +7,8 @@ export type {
   EaCMetadataBase,
   EaCModuleActuator,
   EverythingAsCode,
-} from "jsr:@fathym/eac@0.2.160-hmis";
-export { callEaCActuatorConnections } from "jsr:@fathym/eac@0.2.160-hmis/steward/utils";
+} from "jsr:@fathym/eac@0.2.162-hmis";
+export { callEaCActuatorConnections } from "jsr:@fathym/eac@0.2.162-hmis/steward/utils";
 
 export {
   type EverythingAsCodeClouds,
@@ -25,12 +25,12 @@ export {
   type EaCGitHubAppDetails,
   isEaCGitHubAppAsCode,
   isEaCGitHubAppDetails,
-} from "jsr:@fathym/eac-github@0.0.33-integration";
+} from "jsr:@fathym/eac-github@0.0.34-integration";
 
 export {
   type EaCGitHubAppProviderDetails,
   isEaCGitHubAppProviderDetails,
-} from "jsr:@fathym/eac-identity@0.0.40-integration";
+} from "jsr:@fathym/eac-identity@0.0.41-integration";
 
 export * from "npm:octokit@4.1.2";
 export { type OctokitOptions } from "npm:@octokit/core@6.1.4";
